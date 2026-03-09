9ca50f09f65c3dc97d26e687f6fc59bd2353eb6d
MohammedHashimNilger <hashimrangrezz786@gmail.com>
2026-03-09 09:32:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,13 +12,11 @@ export default function App() {
       setLoading(true); setResult("");
       const fd = new FormData();
       fd.append("resume", file);
-      app.get("/", (req, res) => {
-  res.send("AI Resume Analyzer API is running");
-});
      const res = await fetch("https://ai-resume-analyzer-92gr.onrender.com/analyze", {
-  method: "POST",
-  body: fd
-});if (!res.ok) throw new Error();
+     method: "POST",
+     body: fd
+      });
+      if (!res.ok) throw new Error();
       const data = await res.json();
       setResult(data.result);
     } catch { alert("Something went wrong. Check backend."); }
